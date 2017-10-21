6941bd515cd23b763eb9ca68881d13a03cdeafa8
Ayra Obazee <ayra.panganiban@gmail.com>
2017-10-21 20:43:34 +0800

    add migration for creating access tokens

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021084438) do
+ActiveRecord::Schema.define(version: 20171021120757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "access_tokens", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "answers", force: :cascade do |t|
     t.integer  "response_id"
