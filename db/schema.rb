eac25881aa36402fdfbe0577edb4f09726a94d8f
Ayra Obazee <ayra.panganiban@gmail.com>
2017-10-23 03:50:20 +0800

    add answer to answers migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022170604) do
+ActiveRecord::Schema.define(version: 20171022172054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171022170604) do
     t.integer  "question_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.text     "answer"
     t.index ["question_id"], name: "index_answers_on_question_id", using: :btree
     t.index ["response_id"], name: "index_answers_on_response_id", using: :btree
   end
