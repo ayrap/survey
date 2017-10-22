7a0069c35095f6317d51068f71f1f9f4d3da82df
Ayra Obazee <ayra.panganiban@gmail.com>
2017-10-23 04:29:34 +0800

    add response api

---
+++ app/controllers/responses_controller.rb
@@ -0,0 +1,23 @@
+class ResponsesController < ApiController
+  before_action :get_web_survey, only: [:create]
+
+  def create
+    @response = @web_survey.responses.build obj_params
+    
+    if @response.save
+      render_success
+    else
+      obj_errors @response
+    end
+  end
+
+  private
+
+  def get_web_survey
+    @web_survey = WebSurvey.friendly.find(obj_params[:web_survey_id])
+  end
+
+  def obj_params
+    params.fetch(:response).permit(:web_survey_id, answers_attributes: [:answer, :question_id], respondent_attributes: [:name])
+  end
+end
--- app/controllers/web_surveys_controller.rb
+++ app/controllers/web_surveys_controller.rb
@@ -1,19 +1,16 @@
-class WebSurveysController < ApiController
-
-  # before_action :set_web_survey, only: [:index]
-  
+class WebSurveysController < ApiController 
   def index
-    web_surveys = WebSurvey.all
-    render json: web_surveys
+    @web_surveys = WebSurvey.all
+    render json: @web_surveys
   end
 
   def create
-    web_survey = current_user.web_surveys.new(obj_params)
+    @web_survey = current_user.web_surveys.new(obj_params)
 
-    if web_survey.save
-      render json: WebSurveySerializer.new(web_survey).as_json
+    if @web_survey.save
+      render json: WebSurveySerializer.new(@web_survey).as_json
     else
-      obj_errors web_survey
+      obj_errors @web_survey
     end
   end
 
