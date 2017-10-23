cf2aad24947c8ff7ef2fe88b1d9acd0e43ef0b8d
Ayra Obazee <ayra.panganiban@gmail.com>
2017-10-23 11:25:49 +0800

    toggle responses

--- client/src/components/Home/SurveyItem.js
+++ client/src/components/Home/SurveyItem.js
@@ -4,6 +4,15 @@ class SurveyItem extends Component {
   constructor(props) {
     super(props);
     this.renderResponses = this.renderResponses.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      showResponses: false
+    };
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({showResponses: !this.state.showResponses});
   }
 
   renderResponses(answers) {
@@ -17,28 +26,36 @@ class SurveyItem extends Component {
     });
   }
 
+  renderRecords() {
+    return (
+      this.props.survey.responses.length > 0 ?
+        this.props.survey.responses.map((response) => {
+          return (
+            <tr key={response.id} className="bg-info">
+              <td colSpan='3'>
+                <label>Name of Respondent: {response.respondent}</label>
+                {this.renderResponses(response.answers)}
+              </td>
+            </tr> 
+          );
+        })
+        :
+        <tr><td colSpan='3'>No respondents yet.</td></tr>
+      );
+  }
+
   render() {
     let { survey } = this.props;
+    let { showResponses } = this.state;
+    
     return (
       <tbody>
-        <tr>
+        <tr onClick={this.handleClick}>
           <td>{survey.id}</td>
           <td>{survey.title}</td>
           <td>{`${window.location.host}/surveys/${survey.slug}`}</td>
         </tr>
-        {survey.responses.length > 0 ?
-          survey.responses.map((response) => {
-            return (
-              <tr key={response.id} className="bg-info">
-                <td colSpan='3'>
-                  <label>Name of Respondent: {response.respondent}</label>
-                  {this.renderResponses(response.answers)}
-                </td>
-              </tr> 
-            );
-          })
-          :
-          <tr><td colSpan='3'>No respondents yet.</td></tr>}
+        {showResponses && this.renderRecords()}
       </tbody>
     );
   }
